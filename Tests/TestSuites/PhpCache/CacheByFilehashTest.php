386602e4381a867b478fe7a6ebef5fd7b46b4c77
Anthony Van de Gejuchte <anthonyvdg@yahoo.com>
2011-04-25 19:50:30 +0200

    Move locations files together

--- Tests/TestSuites/PhpCache/CacheByFilehashTest.php
+++ Tests/TestSuites/PhpCache/CacheByFilehashTest.php
@@ -6,12 +6,30 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
 {
     private $_cache;
     private $_cleanupMocks = false;
+    private $_dir         = array();
+    private $_file        = array();
     private $_path;
 
     public function setUp()
     {
         $this->_cache = new CacheByFileHash();
         $this->_path   = dirname(__FILE__) . '/../_mocks';
+
+        $this->_dir = array(
+            'cache'                => $this->_path . '/cache',
+            'cacheExtraDir'        => $this->_path . '/cache/dir',
+            'files'                => $this->_path . '/files',
+        );
+
+        $this->_file = array(
+            'bar'        => $this->_path . '/files/bar.txt',
+            'foo'        => $this->_path . '/files/foo.txt',
+            'barCache'   => $this->_path . '/cache/b277e0b295e6982a57731ef4068eebd950bcf741',
+            'fooCache'   => $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41',
+            'wrongFile1' => $this->_path . '/cache/ga13413415465655244552451443353413434142',
+            'wrongFile2' => $this->_path . '/cache/1343545ab15452f15452454524526452524545545',
+            'wrongFile3' => $this->_path . '/cache/14134135152452',
+        );
     }
 
     public function tearDown()
@@ -26,7 +44,7 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
         $this->_cleanupMocks = true;
 
         $this->_cache->setConfig(array(
-            'cacheDirectory' => $this->_path . '/cache',
+            'cacheDirectory' => $this->_dir['cache'],
         ));
     }
 
@@ -34,14 +52,18 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
     {
         $this->_cleanupMocks = false;
 
-        $iterator = new DirectoryIterator($this->_path . '/cache');
+        $iterator = new DirectoryIterator($this->_dir['cache']);
 
         foreach ($iterator as $directoryItem) {
             if ($directoryItem->isDir()) {
                 continue;
             }
 
-            unlink($this->_path . '/cache/' . $directoryItem->getFileName());
+            unlink($this->_dir['cache'] . '/' . $directoryItem->getFileName());
+        }
+
+        if (is_dir($this->_dir['cacheExtraDir'])) {
+            rmdir($this->_dir['cacheExtraDir']);
         }
     }
 
@@ -50,10 +72,10 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
         $this->assertEquals(false, $this->_cache->getConfig('cacheDirectory'));
 
         $this->_cache->setConfig(array(
-            'cacheDirectory' => $this->_path . '/cache',
+            'cacheDirectory' => $this->_dir['cache'],
         ));
 
-        $this->assertEquals($this->_path . '/cache',
+        $this->assertEquals($this->_dir['cache'],
             $this->_cache->getConfig('cacheDirectory')
         );
     }
@@ -62,7 +84,7 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
     {
         // temp fix because false positive
         $this->_cache->setConfig(
-            array('cacheDirectory' => $this->_path . '/cache')
+            array('cacheDirectory' => $this->_dir['cache'])
         );
 
         $this->assertEquals(false, $this->_cache->getConfig('unknow'));
@@ -72,7 +94,7 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
     public function testGetHashFile()
     {
         $this->assertEquals('4306b576bb5db0d0304b6266aaab5fb1d27cae41',
-            $this->_cache->getHashFile($this->_path . '/files/foo.txt')
+            $this->_cache->getHashFile($this->_file['foo'])
         );
     }
 
@@ -80,112 +102,94 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
     {
         $this->setUpMocks();
 
-        $file  = $this->_path . '/files/foo.txt';
-        $cache = $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41';
-
-        $this->assertEquals($cache, $this->_cache->getObjectLocation($file));
+        $this->assertEquals($this->_file['fooCache'],
+            $this->_cache->getObjectLocation($this->_file['foo'])
+        );
     }
 
     public function testAddCacheWithIsCacheHit()
     {
         $this->setUpMocks();
 
-        $file  = $this->_path . '/files/foo.txt';
-        $cache = $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41';
-        $this->assertEquals(false, file_exists($cache));
-        $this->assertEquals(false, $this->_cache->isCacheHit($file));
+        $this->assertEquals(false, file_exists($this->_file['fooCache']));
+        $this->assertEquals(false, $this->_cache->isCacheHit($this->_file['foo']));
 
-        $this->_cache->addCache($file, 'foo');
-        $this->assertEquals(true, file_exists($cache));
-        $this->assertEquals(true, $this->_cache->isCacheHit($file));
+        $this->_cache->addCache($this->_file['foo'], 'foo');
+        $this->assertEquals(true, file_exists($this->_file['fooCache']));
+        $this->assertEquals(true, $this->_cache->isCacheHit($this->_file['foo']));
     }
 
     public function testGetCache()
     {
         $this->setUpMocks();
 
-        $file  = $this->_path . '/files/foo.txt';
-        $cache = $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41';
-        $this->assertEquals(false, $this->_cache->getCache($file));
+        $this->assertEquals(false, $this->_cache->getCache($this->_file['foo']));
 
-        $this->_cache->addCache($file, 'foo');
-        $this->assertEquals('foo', $this->_cache->getCache($file));
+        $this->_cache->addCache($this->_file['foo'], 'foo');
+        $this->assertEquals('foo', $this->_cache->getCache($this->_file['foo']));
     }
 
     public function testKeep()
     {
         $this->setUpMocks();
 
-        $bar        = $this->_path . '/files/bar.txt';
-        $foo        = $this->_path . '/files/foo.txt';
-        $barCache   = $this->_path . '/cache/b277e0b295e6982a57731ef4068eebd950bcf741';
-        $fooCache   = $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41';
-        $wrongFile1 = $this->_path . '/cache/ga13413415465655244552451443353413434142';
-        $wrongFile2 = $this->_path . '/cache/1343545ab15452f15452454524526452524545545';
-        $wrongFile3 = $this->_path . '/cache/14134135152452';
-        $dir        = $this->_path . '/cache/dir';
+        file_put_contents($this->_file['wrongFile1'], 'Important');
+        file_put_contents($this->_file['wrongFile2'], 'Important');
+        file_put_contents($this->_file['wrongFile3'], 'Important');
 
-        file_put_contents($wrongFile1, 'Important');
-        file_put_contents($wrongFile2, 'Important');
-        file_put_contents($wrongFile3, 'Important');
-        mkdir($dir);
+        mkdir($this->_dir['cacheExtraDir']);
 
-        $this->_cache->addCache($bar, 'bar');
-        $this->_cache->addCache($foo, 'foo');
-        $this->assertEquals(true, file_exists($barCache));
-        $this->assertEquals(true, file_exists($fooCache));
+        $this->_cache->addCache($this->_file['bar'], 'bar');
+        $this->_cache->addCache($this->_file['foo'], 'foo');
+        $this->assertEquals(true, file_exists($this->_file['barCache']));
+        $this->assertEquals(true, file_exists($this->_file['fooCache']));
 
-        $this->_cache->keep(array($this->_cache->getHashFile($bar)));
-        $this->assertEquals(true,  file_exists($barCache));
-        $this->assertEquals(false, file_exists($fooCache));
+        $this->_cache->keep(array(
+            $this->_cache->getHashFile($this->_file['bar'])
+        ));
+        $this->assertEquals(true,  file_exists($this->_file['barCache']));
+        $this->assertEquals(false, file_exists($this->_file['fooCache']));
 
-        $this->assertEquals(true, file_exists($wrongFile1));
-        $this->assertEquals(true, file_exists($wrongFile2));
-        $this->assertEquals(true, file_exists($wrongFile3));
-        $this->assertEquals(true, is_dir($dir));
+        $this->assertEquals(true, file_exists($this->_file['wrongFile1']));
+        $this->assertEquals(true, file_exists($this->_file['wrongFile2']));
+        $this->assertEquals(true, file_exists($this->_file['wrongFile3']));
+        $this->assertEquals(true, is_dir($this->_dir['cacheExtraDir']));
 
-        rmdir($dir);
+        rmdir($this->_dir['cacheExtraDir']);
     }
 
     public function testClean()
     {
         $this->setUpMocks();
 
-        $bar      = $this->_path . '/files/bar.txt';
-        $foo      = $this->_path . '/files/foo.txt';
-        $barCache = $this->_path . '/cache/b277e0b295e6982a57731ef4068eebd950bcf741';
-        $fooCache = $this->_path . '/cache/4306b576bb5db0d0304b6266aaab5fb1d27cae41';
-
-        $this->_cache->addCache($bar, 'bar');
-        $this->_cache->addCache($foo, 'foo');
-        $this->assertEquals(true, file_exists($barCache));
-        $this->assertEquals(true, file_exists($fooCache));
+        $this->_cache->addCache($this->_file['bar'], 'bar');
+        $this->_cache->addCache($this->_file['foo'], 'foo');
+        $this->assertEquals(true, file_exists($this->_file['barCache']));
+        $this->assertEquals(true, file_exists($this->_file['fooCache']));
 
         $this->_cache->clean();
-        $this->assertEquals(false, file_exists($barCache));
-        $this->assertEquals(false, file_exists($fooCache));
+        $this->assertEquals(false, file_exists($this->_file['barCache']));
+        $this->assertEquals(false, file_exists($this->_file['fooCache']));
     }
 
     public function testExpectExceptions()
     {
-        $pathFiles = $this->_path . '/../files';
-
         try {
-            $this->_cache->addCache($pathFiles . '/foo.txt', 'foo');
+            $this->_cache->addCache($this->_file['foo'], 'foo');
             $this->fail();
         } catch (Exception $e) {
             $this->assertEquals('Cache directory not set', $e->getMessage());
         }
 
         try {
-            $this->_cache->getCache($pathFiles . '/foo.txt');
+            $this->_cache->getCache($this->_file['foo']);
             $this->fail();
         } catch (Exception $e) {
             $this->assertEquals('Cache directory not set', $e->getMessage());
         }
 
         try {
-            $this->_cache->isCacheHit($pathFiles . '/foo.txt');
+            $this->_cache->isCacheHit($this->_file['foo']);
             $this->fail();
         } catch (Exception $e) {
             $this->assertEquals('Cache directory not set', $e->getMessage());
@@ -199,14 +203,14 @@ class CacheFilehashTest extends PHPUnit_Framework_TestCase
         }
 
         try {
-            $this->_cache->keep(array($pathFiles . '/foo.txt'));
+            $this->_cache->keep(array($this->_file['foo']));
             $this->fail();
         } catch (Exception $e) {
             $this->assertEquals('Cache directory not set', $e->getMessage());
         }
 
         try {
-            $this->_cache->getObjectLocation($pathFiles . '/foo.txt');
+            $this->_cache->getObjectLocation($this->_file['foo']);
             $this->fail();
         } catch (Exception $e) {
             $this->assertEquals('Cache directory not set', $e->getMessage());
